cb9537d9f2d9952bc293a185b0ef2fc024754f44
Sebbito <eric.bortz124@gmail.com>
2022-11-01 10:44:36 +0100

    added empty functions for listing and adding filed to sync control

--- src/sync.py
+++ src/sync.py
@@ -3,27 +3,58 @@
 import filecmp as fc
 import os
 
+
 def needs_syncing(path1, path2):
+
     '''
     Checks if a file needs syncing or not.
+
     Arguments:
         path1 & path2: Path to the file as String
+
     Return value:
         Path to file that is outdated or none.
     '''
-    
+
     ret = fc.cmp(path1, path2)
 
-    if (ret == False): # files are different
-        
+    if (ret is False):  # files are different
+
         fs1 = os.stat(path1)
         fs2 = os.stat(path2)
 
         # sync the one which has a later modificaton date
-        if (fs1.st_mtime > fs2.st_mtime): 
+        if (fs1.st_mtime > fs2.st_mtime):
             return path2
         else:
             return path1
 
-    return ret;
+    return ret
 
+
+def list_files():
+    '''
+    Lists files that are under tracked for syncing.
+
+    Arguments:
+        none
+
+    Return value:
+        String which lists all files with path
+    '''
+
+    raise 'Not implemented yet'
+
+
+def add_file(path):
+    '''
+    Adds a file to tracking list.
+
+    Argument:
+        Path to the file.
+
+    Return value:
+        True if it succeeded, False otherwise
+    '''
+
+    raise 'Not implemented yet'
