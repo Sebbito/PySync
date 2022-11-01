0d37a923d927426ea2c6bbff96e8eecabf87248d
Sebbito <eric.bortz124@gmail.com>
2022-11-01 10:55:01 +0100

    added more command line options

--- src/pysync.py
+++ src/pysync.py
@@ -1,5 +1,8 @@
 # This file implements functions and ressources for the client side of pysync
 import argparse as ap
+import sync
+
+VERSION='0.0.1'
 
 CONF_PATH = '~/.config/pysync/client'
 
@@ -8,4 +11,8 @@ parser = ap.ArgumentParser(
         description='Client Program for file synchronization with a PySync server instance.'
         )
 
-parser.print_help()
+parser.add_argument('-v', '--version', action='version', version='%(prog)s {}'.format(VERSION) )
+parser.add_argument('-l', '--list', action=sync.list_files(), help='Lists all files that are synced')
+# parser.print_help()
+args = parser.parse_args()
+print(vars(args))
