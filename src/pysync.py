85ab722e7094c8e0184a98dc313495ddff9707d6
Sebbito <eric.bortz124@gmail.com>
2022-12-12 19:22:10 +0100

    changed arguments and added logic for listening and sending a single file

--- src/pysync.py
+++ src/pysync.py
@@ -1,19 +1,33 @@
+#!/bin/python3
 # This file implements functions and ressources for the client side of pysync
 import argparse as ap
 import filehelper
+from receiver import Receiver
+from sender import Sender
 
 VERSION='0.0.1'
 
-CONF_PATH = '~/.config/pysync/client'
+# CONF_PATH = '~/.config/pysync/client'
 
 parser = ap.ArgumentParser(
         prog='pysync',
-        description='Client Program for file synchronization with a PySync server instance.'
+        description='Program for file synchronization with another machine running PySync.'
         )
 
 parser.add_argument('-v', '--version', action='version', version='%(prog)s {}'.format(VERSION) )
-parser.add_argument('-l', '--list', action=filehelper.list_files(), help='Lists all files that are tracked for syncing.')
-parser.add_argument('-c', '--check', action=filehelper.check_files(), help='Checks if all tracked files are up to date.')
-# parser.print_help()
+# parser.add_argument('-l', '--list', action=filehelper.list_files(), help='Lists all files that are tracked for syncing.')
+# parser.add_argument('-c', '--check', action=filehelper.check_files(), help='Checks if all tracked files are up to date.')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--listen', action='store_true', help='Opens a port and listens for incoming files.')
+group.add_argument('--send', dest='path', help='Sends files for the given directory.')
+
 args = parser.parse_args()
-print(vars(args))
+print(args)
+
+if args.listen:
+    r = Receiver()
+    r.receive_file()
+if args.path:
+    s = Sender()
+    s.send(args.path)
+
