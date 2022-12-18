e3f0af05a68ad431c25149c055a536260a5a9dc5
Eric Bortz <e.bortz@netstack.de>
2022-12-18 18:29:48 +0100

    in this commit sending and receiving multiple files works. The directories are not created but it still works
    Signed-off-by: Eric Bortz <e.bortz@netstack.de>

--- src/receiver.py
+++ src/receiver.py
@@ -11,22 +11,37 @@ class Receiver(object):
         self.socket = s.socket()
         self.socket.bind((self.address, self.port))
         self.SEPARATOR = "[SEP]"
-        self.BUFFER_SIZE = 4096
+        self.BUFFER_SIZE = 1024
 
     def __exit__(self, *args):
         self.socket.close()
 
-    def receive_file(self):
-        SEPARATOR= self.SEPARATOR
-        BUFFER_SIZE = self.BUFFER_SIZE
-        
+    def receive_all(self):
         self.socket.listen(5)
         client_socket, address = self.socket.accept()
+        print(f"Accepted connection {client_socket} with address {address}")
+
+        file_count = client_socket.recv(self.BUFFER_SIZE).decode()
+        print(f"[i] Receiving {file_count} files")
+        for _ in range(int(file_count)):
+            self.socket.listen(5)
+            client_socket, address = self.socket.accept()
+            self._receive_file(client_socket)
+
+        # close the client socket
+        client_socket.close()
+        # close the server socket
+        self.socket.close()
+
+
+    def _receive_file(self, client_socket):
+        # self.socket.listen(5)
+        # client_socket, address = self.socket.accept()
 
         # receive the file infos
         # receive using client socket, not server socket
-        received = client_socket.recv(BUFFER_SIZE).decode()
-        filename, filesize = received.split(SEPARATOR)
+        received = client_socket.recv(self.BUFFER_SIZE).decode()
+        filename, filesize = received.split(self.SEPARATOR)
         # remove absolute path if there is
         filename = os.path.basename(filename)
         # convert to integer
@@ -38,7 +53,7 @@ class Receiver(object):
         with open(filename, "wb") as f:
             while True:
                 # read 1024 bytes from the socket (receive)
-                bytes_read = client_socket.recv(BUFFER_SIZE)
+                bytes_read = client_socket.recv(self.BUFFER_SIZE)
                 if not bytes_read:    
                     # nothing is received
                     # file transmitting is done
@@ -48,8 +63,4 @@ class Receiver(object):
                 # update the progress bar
                 progress.update(len(bytes_read))
 
-        # close the client socket
-        client_socket.close()
-        # close the server socket
-        self.socket.close()
-        return filename
+        # return filename
--- src/sender.py
+++ src/sender.py
@@ -3,35 +3,65 @@ import tqdm
 from pathlib import Path
 import socket as s
 import os
+from generator import Generator
 
 class Sender:
     def __init__(self):
         self.port = 8008
         self.address = "0.0.0.0"
         self.socket = s.socket()
-        self.BUFFER_SIZE = 4096
+        self.BUFFER_SIZE = 1024
         self.SEPARATOR = "[SEP]"
 
     def send(self, path):
+        '''
+        Scans path and sends all found files through it's dedicated socket.
+        '''
         try:
             p = Path(path)
-            if p.is_file():
-                self.send_file(p.name)
-            else:
-                print("[!] Sending whole dirs is not supportet yet")
-                raise
+            file_counter = self.count_files(p)
+            print(f"[i] Found {file_counter} files.")
+            
+            print("[I] Connecting to socket")
+            self.socket.connect((self.address, self.port))
+            print("[+] Connected to socket")
+
+            self.socket.send(f"{file_counter}".encode())
+            # close the socket
+            self.socket.close() # closing and reconnecting to prevent merged output
+            self.loop_through_and_send(p)
+
+            # close the socket
+            self.socket.close()
+            print("[+] Closed socket")
         except Exception as e:
             print(e)
             raise
+
+    def loop_through_and_send(self, path):
+        p = Path(path)
+        # send the file
+        if p.is_file():
+            # generate a new socket for each file
+            self.socket = s.socket()
+            self.socket.connect((self.address, self.port))
+            self.send_file(p)
+            self.socket.close()
+        # recursion if it is a directory
+        elif p.is_dir():
+            # count files so we can inform the receiver how many we send
+            # send files
+            for file in p.iterdir():
+                print(f"[i] Iterating {file} for dir {p}")
+                self.loop_through_and_send(file)
+        else:
+            print(f"[!] Path '{p}' is neither file nor directory, aborting.")
+            exit()
 
     def send_file(self, filename):
         filesize = os.path.getsize(filename)
         BUFFER_SIZE = self.BUFFER_SIZE
         SEPARATOR= self.SEPARATOR
-
-        print("[I] Connecting to socket")
-        self.socket.connect((self.address, self.port))
-        print("[+] Connected to socket")
 
         # send the filename and filesize
         self.socket.send(f"{filename}{SEPARATOR}{filesize}".encode())
@@ -49,5 +79,16 @@ class Sender:
                 self.socket.sendall(bytes_read)
                 # update the progress bar
                 progress.update(len(bytes_read))
-        # close the socket
-        self.socket.close()
+
+    def count_files(self, path) -> int:
+        ''' Uses recursion to count all files in path. '''
+        counter = 0
+        p = Path(path)
+
+        if p.is_dir():
+            for file in p.iterdir():
+                counter += self.count_files(file)
+        else:
+            counter += 1
+
+        return counter
