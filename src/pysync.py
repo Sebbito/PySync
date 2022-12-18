74208eb67c6a0df7d4425594bf5dbd1023485fdc
Eric Bortz <e.bortz@netstack.de>
2022-12-18 21:29:05 +0100

    nicer prompts and help prompt if no option was chosen
    Signed-off-by: Eric Bortz <e.bortz@netstack.de>

--- src/pysync.py
+++ src/pysync.py
@@ -1,8 +1,8 @@
 #!/bin/python3
 # This file implements functions and ressources for the client side of pysync
 import argparse as ap
-from receiver import Receiver
-from sender import Sender
+import receiver
+import sender 
 
 VERSION='0.0.1'
 
@@ -17,16 +17,16 @@ parser.add_argument('-v', '--version', action='version', version='%(prog)s {}'.f
 # parser.add_argument('-l', '--list', action=filehelper.list_files(), help='Lists all files that are tracked for syncing.')
 # parser.add_argument('-c', '--check', action=filehelper.check_files(), help='Checks if all tracked files are up to date.')
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('--receive', action='store_true', help='Opens a port and receives incoming files.')
-group.add_argument('--send', dest='path', help='Sends files for the given directory.')
+group.add_argument('-r', '--receive', action='store_true', help='Opens a port and receives incoming files, storing them in the current directory.')
+group.add_argument('-s', '--send', dest='path', nargs=1 ,help='Sends files for the given directory to the server.')
 
 args = parser.parse_args()
-print(args)
 
-if args.receive:
-    r = Receiver()
+if args.receive == True:
+    r = receiver.Receiver()
     r.receive_all()
-if args.path:
-    s = Sender()
+elif args.path != None:
+    s = sender.Sender()
     s.send(args.path)
-
+else:
+    parser.print_help()
