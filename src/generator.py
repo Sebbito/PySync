37fd469872edc43e71d768887be5b2cbfa9aecf1
Eric Bortz <e.bortz@netstack.de>
2022-12-18 21:20:35 +0100

    added generator to gereate file list
    Signed-off-by: Eric Bortz <e.bortz@netstack.de>

---
+++ src/generator.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import os, sys
+from difflib import *
+from pathlib import Path
+
+READ_BUF = 512
+
+class Generator(object):
+    # def __init__(self):
+
+    # def __exit__(self, *args):
+
+    def generate_file_list(self, mixed):
+        ''' Generates a list of files from the mixed object.
+            mixed could be a single file or multiple directories
+            We will return all the files as a list of paths '''
+        
+        if mixed == None:
+            print("[!] File list is empty. Aborting")
+            exit()
+        
+        file_list = []
+
+        for path in mixed:
+            self.fetch_files(path, file_list)
+
+        return file_list
+
+    def fetch_files(self, path, list):
+        ''' Uses recursion to add all files in path to the list.'''
+
+        p = Path(path)
+        if p.exists():
+            if p.is_dir():
+                for file in p.iterdir():
+                    self.fetch_files(file, list)
+            elif p.is_file():
+                list.append(p)
+            else:
+                print("[!] Path {p} is not viable. Aborting")
+                exit()
+        else:
+            print("[!] Path {p} is not viable. Aborting")
+            exit()
+
