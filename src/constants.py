7e663686db27fee26e71b23d01352cbaa9bd9211
Sebbito <eric.bortz124@gmail.com>
2022-12-19 14:46:20 +0100

    added skip indicator
    Signed-off-by: Sebbito <eric.bortz124@gmail.com>

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,12 @@
 DEFAULT_PORT = 8008
 DEFAULT_SERVER = "0.0.0.0"
 BUFFER_SIZE = 1024
+
+# Protocol stuff
 SEPARATOR = "[SEP]"
 OK = "[OK]"
+SKIP = "[SKIP]"
+
+# exit codes
 EXIT_SUCCESS = 0
 EXIT_FAILURE = 0
