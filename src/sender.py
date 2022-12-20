e9ea4db13b57fcc38000999f7a3040b89ae6a0fc
Sebbito <eric.bortz124@gmail.com>
2022-12-20 11:47:13 +0100

    rewired all of the functions to be compatible with v4 of the protocol

--- src/sender.py
+++ src/sender.py
@@ -8,78 +8,88 @@ import receiver
 # whenever you see constants, they are from here
 from constants import *
 
-class Sender:
-    def __init__(self, server_address = DEFAULT_SERVER, server_port = DEFAULT_PORT, update = False):
-        self.port = server_port
-        self.address = server_address
-        self.socket = None
-        self.update = update
+# class Sender:
+#     def __init__(self, server_address = DEFAULT_SERVER, server_port = DEFAULT_PORT, update = False):
+#         self.port = server_port
+#         self.address = server_address
+#         self.socket = None
+#         self.update = update
 
-    def __exit__(self, *args):
-        self.socket.close()
+#     def __exit__(self, *args):
+#         self.socket.close()
 
-    def send(self, mixed):
-        '''
-        Sends all files in the given Path 'mixed'. Each file is sent through it's own socket.
-        '''
-        try:
-            file_list = gen.generate_file_list(mixed)
-            file_count = len(file_list)
+def send(args):
+    '''
+    Sends all files in the given Path 'mixed'. Each file is sent through it's own socket.
+    '''
+    try:
+        file_list = gen.generate_file_list(args['file'])
+        file_count = len(file_list)
 
-            print(f"[i] Found {file_count} files.")
-            
-            initiate_communication(address=self.address, port=self.port, update=self.update, file_count=file_count)
+        print(f"[i] Found {file_count} files.")
+        
+        initiate_communication(DEFAULT_SERVER, DEFAULT_PORT, args)
 
-            # send all the files in the file list
-            self.loop_through_and_send(file_list)
+        # send all the files in the file list
+        loop_through_and_send(DEFAULT_SERVER, DEFAULT_PORT, file_list)
 
-            print("[+] Closed socket")
-        except Exception as e:
-            print(e)
-            raise
+        print("[+] Closed socket")
+    except Exception as e:
+        print(e)
+        raise
 
-    def loop_through_and_send(self, file_list):
-        for path in file_list:
-            # send the file
-            if path.exists() and path.is_file:
-                # generate a new socket for each file
-                # closing and reconnecting to prevent merged output
-                self.socket = s.socket()
-                self.socket.connect((self.address, self.port))
-                
-                send_file(self.socket, path)
-                
-                self.socket.close()
-            else:
-                print(f"[!] Path '{p}' is not a file or doesn't exists")
-                exit()
 
-def initiate_communication(address, port, update=False, file_count=0, intention='send'):
-    if intention == 'send':
-        try:
+def initiate_communication(address, port, args):
+    ''' Sends the arguments over to the server to set it up for transmition. '''
+    try:
+        socket = s.socket()
+        print(f"[i] Connecting to server {address}:{port}.")
+        socket.connect((address, port))
+        print("[+] Connected to server.")
+
+        socket.send(f"{file_count}{SEPARATOR}{update}".encode())
+
+        rec = socket.recv(BUFFER_SIZE).decode()
+
+        if not rec == OK:
+            print(f"[!] Invalid server response '{rec}'. Aborting")
+            exit(EXIT_FAILURE)
+
+        print(f"[i] Received ok. Start sending files.")
+        # close the socket
+        socket.close()
+    except Exception as e:
+        print("[!] Server not found.")
+        raise e
+
+def loop_through_and_send(address, port, file_list):
+    ''' Opens sockets for each file and sends the file contents to the server. '''
+
+    socket.send(f"{file_count}".encode())
+
+    rec = socket.recv(BUFFER_SIZE).decode()
+
+    if not rec == OK:
+        print(f"[!] Invalid server response '{rec}'. Aborting")
+        exit(EXIT_FAILURE)
+
+    print(f"[i] Received ok. Start sending files.")
+
+    for path in file_list:
+        # send the file
+        if path.exists() and path.is_file:
+            # generate a new socket for each file
+            # closing and reconnecting to prevent merged output
             socket = s.socket()
-            print(f"[i] Connecting to server {address}:{port}.")
             socket.connect((address, port))
-            print("[+] Connected to server.")
 
-            socket.send(f"{file_count}{SEPARATOR}{update}".encode())
-
-            rec = socket.recv(BUFFER_SIZE).decode()
-
-            if not rec == OK:
-                print(f"[!] Invalid server response '{rec}'. Aborting")
-                exit(EXIT_FAILURE)
-
-            print(f"[i] Received ok. Start sending files.")
+            send_file(socket, path)
+            
             # close the socket
             socket.close()
-        except Exception as e:
-            print("[!] Server not found.")
-            raise e
-
-    else:
-        print(f"[!] Invalid intention. Aborting")
-        exit(EXIT_FAILURE)
+        else:
+            print(f"[!] Path '{p}' is not a file or doesn't exists")
+            exit()
     
 def send_file(socket, file):
     # prepare first info pack
@@ -98,10 +108,6 @@ def send_file(socket, file):
         send_file_contents(socket, file)
     elif received == SKIP:
         print("[i] Received skip, file contents are the same, continuing.")
-    elif received == UPDATE:
-        print("[i] Received update, server has newer files, continuing.")
-        socket.send(f"{OK}".encode())
-        receiver.receive_file(socket)
     else:
         print("[!] No status signal received. Aborting file transmission!")
         exit()
