cd063bd52a153180efb1b3b1bc95ede6774e5c49
Sebbito <eric.bortz124@gmail.com>
2022-12-20 11:46:54 +0100

    redid a lot of the functons and added subfunctions for getting and syncing. Functionality not there yet

--- src/pysync.py
+++ src/pysync.py
@@ -5,9 +5,9 @@ import receiver
 import sender 
 from constants import *
 
-VERSION='0.0.1'
+VERSION="0.0.1"
 
-# CONF_PATH = '~/.config/pysync/client'
+# CONF_PATH = "~/.config/pysync/client"
 
 def receive(args):
     if args.destination:
@@ -25,40 +25,65 @@ def receive(args):
     exit(EXIT_SUCCESS)
 
 def send(args):
-    s = sender.Sender(update=args.update)
-    if args.file:
-        s.send(args.file)
+    # s = sender.Sender(update=args.update)
+    sender.send(args)
+    exit(EXIT_SUCCESS)
+
+def get(args):
+    # s = sender.Sender(update=args.update)
+    sender.get(args)
+    exit(EXIT_SUCCESS)
+
+def sync(args):
+    # s = sender.Sender(update=args.update)
+    sender.sync(args)
     exit(EXIT_SUCCESS)
 
 if __name__ == "__main__":
     parser = ap.ArgumentParser(
-            prog='pysync',
-            description='Program for file synchronization with another machine running PySync.'
+            prog="pysync",
+            description="Program for file synchronization with another machine running PySync."
             )
 
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s {}'.format(VERSION) )
-    # parser.add_argument('-l', '--list', action=filehelper.list_files(), help='Lists all files that are tracked for syncing.')
-    # parser.add_argument('-c', '--check', action=filehelper.check_files(), help='Checks if all tracked files are up to date.')
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s {}".format(VERSION) )
+    # parser.add_argument("-l", "--list", action=filehelper.list_files(), help="Lists all files that are tracked for syncing.")
+    # parser.add_argument("-c", "--check", action=filehelper.check_files(), help="Checks if all tracked files are up to date.")
     # group = parser.add_mutually_exclusive_group(required=True)
-    subparsers = parser.add_subparsers(help='pysync {command} --help')
+    subparsers = parser.add_subparsers(help="pysync {command} --help")
+
+    # Syncing
+    parser_send = subparsers.add_parser("sync", aliases=["sy"], help="Synchronizes all files. Similar to send and get with -uc options.")
+    parser_send.add_argument("-c", "--create", action="store_true", help="Create non-existant files and directories.")
+    parser_send.set_defaults(func=send)
+
+    # Getting
+    parser_send = subparsers.add_parser("get", aliases=["ge"], help="Pulls all files from server. Overwrites local files with the same name on default.")
+    parser_send.add_argument("-u", "--update", action="store_true", help="Update local files. Older remote files are skipped.")
+    parser_send.add_argument("-c", "--create", action="store_true", help="Create non-existant files and directories.")
+    parser_send.set_defaults(func=send)
 
     # Sending
-    parser_send = subparsers.add_parser('send', aliases=['s'], help='Sends files from the given directory to the server.')
-    parser_send.add_argument('file', nargs='+', help='Files to be sent.')
-    parser_send.add_argument('-u', '--update', action='store_true', help='Update local files if server has newer changes.')
+    parser_send = subparsers.add_parser("send", aliases=["se"], help="Sends files from the given directory to the server. Overwrites local files with the same name on default.")
+    parser_send.add_argument("file", nargs="+", help="Files to be sent.")
+    parser_send.add_argument("-u", "--update", action="store_true", help="Update remote files. Older local files are skipped.")
+    parser_send.add_argument("-c", "--create", action="store_true", help="Create non-existant files and directories.")
     parser_send.set_defaults(func=send)
 
     # Receiving 
-    parser_receive = subparsers.add_parser('receive', aliases=['r'], help="Opens a port and receives incoming files, storing them in the current directory.")
-    parser_receive.add_argument('-o', '--once', action='store_true', help='Only do one transaction, then quit, closing the port.')
-    parser_receive.add_argument('-c', '--continuos', action='store_true', help='Continuosly receive files, ending only when killed.')
-    parser_receive.add_argument('-d', '--destination', help='Directory where the files should be stored.')
+    parser_receive = subparsers.add_parser("receive", aliases=["re"], help="Opens a port and receives incoming files, storing them in the current directory.")
+    parser_receive.add_argument("-o", "--once", action="store_true", help="Only do one transaction, then quit, closing the port.")
+    parser_receive.add_argument("-c", "--continuos", action="store_true", help="Continuosly receive files, ending only when killed.")
+    parser_receive.add_argument("-d", "--destination", help="Chose a different directory for file storage.")
     parser_receive.set_defaults(func=receive)
 
     args = parser.parse_args()
-    print(args)
+    print(f"Args: {args}")
 
-    if hasattr(args, 'func'):
-        args.func(args)
+    if hasattr(args, "func"):
+        func = args.func
+        options = vars(args).copy()
+        options["func"] =  options["func"].__name__# this is not needed for any process
+        print(f"{options}")
+        args.func(options)
     else:
         parser.print_help()
